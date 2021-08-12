bf852feb6ac408884304df5cb465e78b00ca9170
RodrigoPaulaLopes <rodrigolopesti1@gmail.com>
2021-08-12 19:13:11 -0300

    CRUD sem banco com react js

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './footer.css'
+import React from 'react'
+
+export default props =>
+     <footer className="footer">
+        <span>
+        <i className="fa fa-code text-danger"></i>  Desenvolvido com  por <strong>Rodrigo Lopes</strong> <i className="fa fa-code text-danger"></i>
+        </span>
+    </footer>   
---
+++ frontend/src/components/template/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../Main'
+
+export default props => 
+        <Main icon="home" title="Inicio" subtitle="Projeto CRUD com Reactjs.">
+           <div className="display-4">Bem vindo!</div>
+           <hr />
+           <p className="mb-0">Sistema de cadastro de usuarios com REACT JS</p>
+        </Main>
